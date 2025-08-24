bf95a45ad28fa136cbb5fa5c6910c9a044e6d306
Syed Daniyal Ali Shah <daniyalashah988@gmail.com>
2025-08-25 00:10:47 +0500

    update 4 files and create 4 files

---
+++ frontend/src/app/components/card-modal/card-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-modal',
+  imports: [],
+  templateUrl: './card-modal.html',
+  styleUrl: './card-modal.css'
+})
+export class CardModal {
+
+}
